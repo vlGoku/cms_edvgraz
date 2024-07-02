e3e68bcb4b68dddd9b32811abdff6a6b8c7370a7
vlGoku <markolucic21@outlook.de>
2024-07-02 09:38:13 +0200

    autoloader for classes, dsn

---
+++ config/config.php
@@ -0,0 +1,18 @@
+<?php
+const DEV_MODE = true;
+const DOC_ROOT = '/cms-system/public/';
+const ROOT_FOLDER = '/public/';
+//Database connection
+$type = 'mysql';
+$host = 'localhost';
+$port = '3306';
+$dbname = 'cms_edvgraz';
+$user_name = 'cms_edvgraz';
+$password = '';
+$dsn = "$type:host=$host:$port;dbname=$dbname";
+
+//File upload
+const MEDIA_TYPES = ['image/jpeg', 'image/png'];
+const FILE_EXTENSIONS = ['jpg', 'jpeg', 'png'];
+const MAX_FILE_SIZE = 1024 * 1024 * 2;
+define ("UPLOAD_DIR", dirname(__DIR__, 1) . DIRECTORY_SEPARATOR . ROOT_FOLDER . DIRECTORY_SEPARATOR . '/uploads/');
